ffdbe095c8bd42fa25e5d130a175f5a81bfed340
jonathan sun <chun.sun@va.gov>
2021-04-19 15:31:28 -0700

    initial commit

---
+++ IPRehabRepository/Contracts/I_IPRehabRepositories.cs
@@ -0,0 +1,37 @@
+﻿using IPRehabModel;
+
+namespace IPRehabRepository.Contracts
+{
+  public interface IAnswerRepository : IRepositoryBase<TblAnswer>
+  {
+  }
+
+  public interface IEpisodeOfCareRepository : IRepositoryBase<TblEpisodeOfCare>
+  {
+  }
+
+  public interface IQuestionRepository : IRepositoryBase<TblQuestion>
+  {
+  }
+
+  public interface IUserRepository : IRepositoryBase<TblUser>
+  {
+  }
+
+  public interface IPatientRepository : IRepositoryBase<TblPatient>
+  {
+  }
+
+  public interface ICodeSetRepository : IRepositoryBase<TblCodeSet>
+  {
+  }
+
+  public interface IQuestionInstructionRepository : IRepositoryBase<TblQuestionInstruction>
+  {
+  }
+
+  public interface ISignatureRepository : IRepositoryBase<TblSignature>
+  {
+  }
+}
+
