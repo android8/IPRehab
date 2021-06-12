3b38848a27917a22af69e8155447d33d6c55afa1
jonathan sun <chun.sun@va.gov>
2021-06-12 00:09:02 -0700

    dynamic scroll banner

---
+++ IPRehab/Scripts/app/animateBanner.js
@@ -0,0 +1,30 @@
+/// <reference path="../../node_modules/@types/jquery/jquery.d.ts" />
+//https://www.typescriptlang.org/docs/handbook/asp-net-core.html
+$(function () {
+    pageLoad();
+});
+function pageLoad() {
+    /*hide header and footer after 2 seconds*/
+    let topScrollY;
+    function getMediaWidth() {
+        let x = window.matchMedia("(max-width:768px)");
+        if (x.matches) {
+            return "-65px";
+        }
+        else {
+            return "-60px";
+        }
+    }
+    topScrollY = getMediaWidth();
+    $('.hoverHeader').css({ 'top': topScrollY, 'z-index': '100', 'transition-duration': '2s' });
+    $('.hoverFooter').css({ 'bottom': '-15px', 'z-index': '100', 'transition-duration': '2s' });
+    $(".hoverHeader, .hoverFooter").hover(function () {
+        $('.hoverHeader').css({ 'top': '0px', 'z-index': '100', 'transition-duration': '1s' });
+        $('.hoverFooter').css({ 'bottom': '0px', 'z-index': '100', 'transition-duration': '1s' });
+    }, function () {
+        topScrollY = getMediaWidth();
+        $('.hoverHeader').css({ 'top': topScrollY, 'z-index': '100', 'transition-duration': '1s' });
+        $('.hoverFooter').css({ 'bottom': '-15px', 'z-index': '100', 'transition-duration': '1s' });
+    });
+}
+//# sourceMappingURL=animateBanner.js.map
---
+++ IPRehab/Scripts/app/animateBanner.ts
@@ -0,0 +1,39 @@
+﻿
+/// <reference path="../../node_modules/@types/jquery/jquery.d.ts" />
+
+//https://www.typescriptlang.org/docs/handbook/asp-net-core.html
+
+$(function () {
+  pageLoad();
+});
+
+function pageLoad(): void {
+  /*hide header and footer after 2 seconds*/
+  let topScrollY: string;
+
+  function getMediaWidth() {
+    let x: any = window.matchMedia("(max-width:768px)");
+    if (x.matches) {
+      return "-65px";
+    }
+    else {
+      return "-60px";
+    }
+  }
+
+  topScrollY = getMediaWidth();
+  $('.hoverHeader').css({ 'top': topScrollY, 'z-index': '100', 'transition-duration': '2s' });
+  $('.hoverFooter').css({ 'bottom': '-15px', 'z-index': '100', 'transition-duration': '2s' });
+
+  $(".hoverHeader, .hoverFooter").hover(
+    function () {
+      $('.hoverHeader').css({ 'top': '0px', 'z-index': '100', 'transition-duration': '1s' });
+      $('.hoverFooter').css({ 'bottom': '0px', 'z-index': '100', 'transition-duration': '1s' });
+    },
+    function () {
+      topScrollY = getMediaWidth();
+      $('.hoverHeader').css({ 'top': topScrollY, 'z-index': '100', 'transition-duration': '1s' });
+      $('.hoverFooter').css({ 'bottom': '-15px', 'z-index': '100', 'transition-duration': '1s' });
+    }
+  );
+}
