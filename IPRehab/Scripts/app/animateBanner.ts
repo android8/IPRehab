3033b7c7899b57d1ebd892c5bfa67e8a80f59a99
jonathan sun <chun.sun@va.gov>
2021-06-15 22:18:12 -0700

    1. dynamically set banner slide off distance on window resize
    2. set margin-top for <main> to 20px  to prevent obscured by banner sliding

--- IPRehab/Scripts/app/animateBanner.ts
+++ IPRehab/Scripts/app/animateBanner.ts
@@ -10,20 +10,40 @@ $(function () {
 function pageLoad(): void {
   /*hide header and footer after 2 seconds*/
   let topScrollY: string;
+  const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+  console.log('view port width', vw);
+  const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+  console.log('view port height', vw);
 
   function getMediaWidth() {
-    let x: any = window.matchMedia("(max-width:768px)");
+    let x = window.matchMedia("(max-width:576px)");
     if (x.matches) {
-      return "-70px";
+      console.log('screen is <= 576 pixels wide');
+      return "-30px";
+    }
+
+    x = window.matchMedia("(max-width:768px)")
+    if (x.matches) {
+      console.log('screen is <= 768 pixels wide');
+      return "-65px";
     }
     else {
-      return "-60px";
+      console.log('screen is >= 768 pixels wide');
+      return "-75px";
     }
   }
 
   topScrollY = getMediaWidth();
+  console.log('documentReady topScrollY', topScrollY);
   $('.hoverHeader').css({ 'top': topScrollY, 'z-index': '100', 'transition-duration': '2s' });
   $('.hoverFooter').css({ 'bottom': '-15px', 'z-index': '100', 'transition-duration': '2s' });
+
+  $(window).resize(function () {
+    topScrollY = getMediaWidth();
+    console.log('mouseOut topScrollY', topScrollY);
+    $('.hoverHeader').css({ 'top': topScrollY, 'z-index': '100', 'transition-duration': '1s' });
+    $('.hoverFooter').css({ 'bottom': '-15px', 'z-index': '100', 'transition-duration': '1s' });
+  });
 
   $(".hoverHeader, .hoverFooter").hover(
     function () {
@@ -32,6 +52,7 @@ function pageLoad(): void {
     },
     function () {
       topScrollY = getMediaWidth();
+      console.log('mouseOut topScrollY', topScrollY);
       $('.hoverHeader').css({ 'top': topScrollY, 'z-index': '100', 'transition-duration': '1s' });
       $('.hoverFooter').css({ 'bottom': '-15px', 'z-index': '100', 'transition-duration': '1s' });
     }
--- IPRehab/wwwroot/js/app/animateBanner.js
+++ IPRehab/wwwroot/js/app/animateBanner.js
@@ -6,23 +6,42 @@ $(function () {
 function pageLoad() {
     /*hide header and footer after 2 seconds*/
     let topScrollY;
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    console.log('view port width', vw);
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    console.log('view port height', vw);
     function getMediaWidth() {
-        let x = window.matchMedia("(max-width:768px)");
+        let x = window.matchMedia("(max-width:576px)");
         if (x.matches) {
+            console.log('screen is <= 576 pixels wide');
+            return "-30px";
+        }
+        x = window.matchMedia("(max-width:768px)");
+        if (x.matches) {
+            console.log('screen is <= 768 pixels wide');
             return "-65px";
         }
         else {
-            return "-60px";
+            console.log('screen is >= 768 pixels wide');
+            return "-75px";
         }
     }
     topScrollY = getMediaWidth();
+    console.log('documentReady topScrollY', topScrollY);
     $('.hoverHeader').css({ 'top': topScrollY, 'z-index': '100', 'transition-duration': '2s' });
     $('.hoverFooter').css({ 'bottom': '-15px', 'z-index': '100', 'transition-duration': '2s' });
+    $(window).resize(function () {
+        topScrollY = getMediaWidth();
+        console.log('mouseOut topScrollY', topScrollY);
+        $('.hoverHeader').css({ 'top': topScrollY, 'z-index': '100', 'transition-duration': '1s' });
+        $('.hoverFooter').css({ 'bottom': '-15px', 'z-index': '100', 'transition-duration': '1s' });
+    });
     $(".hoverHeader, .hoverFooter").hover(function () {
         $('.hoverHeader').css({ 'top': '0px', 'z-index': '100', 'transition-duration': '1s' });
         $('.hoverFooter').css({ 'bottom': '0px', 'z-index': '100', 'transition-duration': '1s' });
     }, function () {
         topScrollY = getMediaWidth();
+        console.log('mouseOut topScrollY', topScrollY);
         $('.hoverHeader').css({ 'top': topScrollY, 'z-index': '100', 'transition-duration': '1s' });
         $('.hoverFooter').css({ 'bottom': '-15px', 'z-index': '100', 'transition-duration': '1s' });
     });
