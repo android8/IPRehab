10c95385ee382863bfa1036344175abdde0beeb8
jonathan sun <chun.sun@va.gov>
2021-06-26 21:29:46 -0700

    1. downgrade node.js to 10+ to avoid gullp error
    2. added auto sass files compilation task as a gulp task
    3. set cookie consent

--- IPRehab/gulpfile.js
+++ IPRehab/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding AfterBuild='cleanDestination, copySelect2Styles, copyJqueryUiStyles, copyDataTableStyles, copyLoadAwesomeStyles, copyBootswatchStyles, copyLibs, copyMyScripts' />
+﻿/// <binding AfterBuild='cleanDestination, copyScripts, compileStyles' />
 
 //https://www.typescriptlang.org/docs/handbook/asp-net-core.html
 
@@ -7,25 +7,36 @@ This file is the main entry point for defining Gulp tasks and using Gulp plugins
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
+var sass = require('gulp-sass')(require('sass'));
 var gulp = require('gulp');
 var del = require('del');
 
-var sources = {
-   myScripts: ['./scripts/**/*.js', './scripts/**/*.ts', './scripts/**/*.map']
- };
+var scriptFiles = {
+  myTypeScripts: ['./Scripts/**/*.js', './Scripts/**/*.ts', './Scripts/**/*.map'],
+  destinations: ['./wwwroot/js']
+};
+
+var styleFiles = {
+  mySass: ['./wwwroot/css/**/*.scss'],
+  destinations: ['./wwwroot/css']
+}
 
 gulp.task('cleanDestination', function () {
-  return del([
-    './wwwroot/css/**/*',
-    './wwwroot/scripts/**/*',
-    './wwwroot/lib/**/*']);
+  return del(['./wwwroot/js/**/*']);
 });
 
-
-gulp.task('copyMyScripts', function () {
-  return gulp.src(sources.myScripts)
-    .pipe(gulp.dest('./wwwroot/scripts'))
+gulp.task('copyScripts', function () {
+  return gulp.src(scriptFiles.myTypeScripts)
+    .pipe(gulp.dest(scriptFiles.destinations))
 });
 
+gulp.task('compileStyles', function (done) {
+  gulp.src(styleFiles.mySass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      gulp.dest(styleFiles.destinations)
+    );
+  done();
+});
 
 
---
+++ IPRehab/wwwroot/js/app/app.ts
@@ -0,0 +1,47 @@
+/// <reference path="../../node_modules/@types/jquery/jquery.d.ts" />
+/// <reference path="../appModels/IUseranswer.ts" />
+
+import { IUserAnswer, AjaxPostbackModel } from "../appModels/IUserAnswer";
+
+//https://www.typescriptlang.org/docs/handbook/asp-net-core.html
+
+$(function () {
+  pageLoad();
+});
+
+function pageLoad(): void {
+  const urlParams: any = new URLSearchParams(window.location.search.substring(1));
+  const previousCriteria: string = urlParams.get('criteria'); //get criteria in the querystring
+  const previousSearchInputValue: string = $("#searchCriteria").val().toString(); //get criteria from the input ellement
+  //$('#previousCriteria').text(previousCriteria);
+
+  if (previousSearchInputValue == '')
+    $('#searchCriteria').val(previousCriteria); //only set the val of the input when the input has no value set by the server
+
+  $('#search').on('click', function () {
+    let host: string = location.host;
+    let searchCriteria: string = $('#searchCriteria').val().toString();
+    let href: string = '';
+    let $this: any = $(this);
+    if (host.indexOf('localhost') != -1) {
+      href = '/Patient/Index?criteria=' + searchCriteria;
+      //alert('local searchCriteria: ' + href);
+    }
+    else {
+      href = '/IPRehabMetrics/Patient/Index?criteria=' + searchCriteria;
+      //alert('remote searchCriteria: ' + href);
+    }
+    //$('#previousCriteria').text('');
+    $('#recordCount').text('');
+    $this.attr('href', href);
+  });
+
+  //handle rehab action checkbox
+  $('#optionCreate').on('click', function () {
+    alert('create option box is checked');
+    let optionCreate: any = $(this).val();
+    if (optionCreate == 'true') {
+      alert('create buttons are set');
+    }
+  })
+}
