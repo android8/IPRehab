e5d489c6f608878316ae134bb3a76cd2c209f27a
jonathan sun <chun.sun@va.gov>
2022-04-03 12:48:51 -0700

    rebased on master

--- IPRehab/Startup.cs
+++ IPRehab/Startup.cs
@@ -24,8 +24,6 @@ namespace IPRehab
       env = environment;
     }
 
-    public IConfiguration Configuration { get; }
-
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
