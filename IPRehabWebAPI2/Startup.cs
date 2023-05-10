104f20e15598eb73a1454a5660512bdf9ed7d71c
Jonathan Sun <chun.sun@va.gov>
2023-05-09 20:21:37 -0700

    1. add rutime override environment variables 2. fix Filed to load API definition

--- IPRehabWebAPI2/Startup.cs
+++ IPRehabWebAPI2/Startup.cs
@@ -33,12 +33,15 @@ namespace IPRehabWebAPI2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            #region custom appsettins
+            #region custom appsettings
+
             //https://www.bing.com/ck/a?!&&p=9f40ffcae2103a86JmltdHM9MTY2MjQyMjQwMCZpZ3VpZD0yMGE2ODFkNi05OTNiLTZiN2YtMThjYS05M2MxOThiZjZhNTEmaW5zaWQ9NTQ0MA&ptn=3&hsh=3&fclid=20a681d6-993b-6b7f-18ca-93c198bf6a51&u=a1aHR0cHM6Ly93d3cuYy1zaGFycGNvcm5lci5jb20vYXJ0aWNsZS9yZWFkaW5nLXZhbHVlcy1mcm9tLWFwcHNldHRpbmdzLWpzb24taW4tYXNwLW5ldC1jb3JlLyM6fjp0ZXh0PVRoZXJlJTIwYXJlJTIwdHdvJTIwbWV0aG9kcyUyMHRvJTIwcmV0cmlldmUlMjBvdXIlMjB2YWx1ZXMlMkMsYXJlJTIwZ2V0dGluZyUyMGFub3RoZXIlMjBzZWN0aW9uJTIwdGhhdCUyMGNvbnRhaW5zJTIwdGhlJTIwdmFsdWUu&ntb=1
             services.Configure<CustomAppSettingsModel>(Configuration.GetSection("AppSettings"));
-            #endregion
+
+            #endregion custom appsettings
 
             #region db setup
+
             string IPRehabConnectionString = Configuration.GetConnectionString("IPRehab");
             string FSODPatientConnectionString = Configuration.GetConnectionString("FSODPatientDetail");
             string MasterReportsConnectionString = Configuration.GetConnectionString("MasterReports");
@@ -54,9 +57,11 @@ namespace IPRehabWebAPI2
             //register the external Masterreports DB context for users
             services.AddDbContext<MasterreportsContext>(
               o => o.UseLazyLoadingProxies().UseSqlServer(MasterReportsConnectionString));
-            #endregion
+
+            #endregion db setup
 
             #region IoC container
+
             services.AddScoped<IQuestionRepository, QuestionRepository>();
             services.AddScoped<IAnswerRepository, AnswerRepository>();
             services.AddScoped<ICodeSetRepository, CodeSetRepository>();
@@ -68,17 +73,22 @@ namespace IPRehabWebAPI2
             services.AddScoped<IQuestionMeasureRepository, QuestionMeasureRepository>();
             services.AddScoped<IFSODPatientRepository, FSODPatientRepository>();
             services.AddScoped<ITreatingSpecialtyPatientRepository, TreatingSpecialtyPatientRepository>();
+
+            //https://learn.microsoft.com/en-us/dotnet/core/extensions/logging-providers
             services.AddScoped<IUserPatientCacheHelper, UserPatientCacheHelper>();
+
 
             /* ToDO: to be deleted after test */
             services.AddScoped<ITestUserPatientCacheHelper, TestUserPatientCacheHelper>();
 
             services.AddScoped<AnswerHelper, AnswerHelper>();
-            #endregion
+
+            #endregion IoC
 
             services.AddAuthentication(IISDefaults.AuthenticationScheme);
 
             #region SWAGGER interface
+
             /* https://docs.microsoft.com/en-us/samples/dotnet/aspnetcore.docs/getstarted-swashbuckle-aspnetcore/?tabs=visual-studio
              * In properties\launchSettimgs.json set 
              * "launchUrl": "api/TreatingSpecialtyPatient to start with TreatingSpecialtyPatient patient page
@@ -99,9 +109,11 @@ namespace IPRehabWebAPI2
                     }
                 });
             });
-            #endregion
+
+            #endregion SWAGGER
 
             #region CORS
+
             //services.AddCors();
             services.AddCors(options =>
             {
@@ -110,15 +122,17 @@ namespace IPRehabWebAPI2
                     policyBuilder =>
                     {
                         policyBuilder
-                        .WithOrigins("https://localhost:44381", "https://vhaausweb3.vha.med.va.gov", "https://https://vaww.vssc.med.va.gov/")
-                        //.AllowAnyOrigin() //comment out for testing on localhost
+                        //.WithOrigins("https://localhost:44381", "https://vhaausweb3.vha.med.va.gov", "https://https://vaww.vssc.med.va.gov/")
+                        .AllowAnyOrigin() //comment out for testing on localhost
                         .AllowAnyHeader()
                         .AllowAnyMethod();
                     });
             });
-            #endregion
+
+            #endregion CORS
 
             #region API Controller behaviors
+
             services.AddControllers(o =>
             {
                 o.EnableEndpointRouting = true;
@@ -136,16 +150,20 @@ namespace IPRehabWebAPI2
              * the factory to change the default response type to SerializableError. */
             .ConfigureApiBehaviorOptions(options =>
             {
+                //The automatic creation of a ProblemDetails for error status codes is disabled when the SuppressMapClientErrors property is set to true:
+                options.SuppressMapClientErrors = false;
+
                 options.InvalidModelStateResponseFactory = context =>
-                {
-                    var result = new BadRequestObjectResult(context.ModelState);
-
-                    result.ContentTypes.Add(MediaTypeNames.Application.Json);
-                    result.ContentTypes.Add(MediaTypeNames.Application.Xml);
-
-                    return result;
-                };
+                    new BadRequestObjectResult(context.ModelState)
+                    {
+                        ContentTypes = {
+                            // using static System.Net.Mime.MediaTypeNames;
+                            MediaTypeNames.Application.Json,
+                            MediaTypeNames.Application.Xml
+                        }
+                    };
             })
+            .AddXmlSerializerFormatters()
             .AddJsonOptions(o =>
               {
                   //preserve circular reference
@@ -153,15 +171,26 @@ namespace IPRehabWebAPI2
                   o.JsonSerializerOptions.WriteIndented = true;
                   o.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
               });
-            #endregion
+            //services.AddProblemDetails(); //only avaialbe in .Net 7 or newer
+            //services.AddTransient<ProblemDetailsFactory, SampleProblemDetailsFactory>();
+
+            #endregion API Controller behaviors
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseStatusCodePages();
+
+            string apiAppSettings = string.Empty;
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
+
+                //or use custom Error controller
+                app.UseExceptionHandler("/error-local-development");
+                apiAppSettings = "D";
             }
             else
             {
@@ -171,14 +200,15 @@ namespace IPRehabWebAPI2
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
+                string swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
 #if DEBUG
                 // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
                 // specifying the Swagger JSON endpoint.
                 // For Debug in Kestrel
-                c.SwaggerEndpoint("/swagger/v4/swagger.json", "Web API V4");
+                c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v4/swagger.json", $"Web API V4 {apiAppSettings}");
 #else
-            // To deploy on IIS
-            c.SwaggerEndpoint("/iprehabmetricswebapi/swagger/v4/swagger.json", "Web API V4");
+                // To deploy on IIS
+                c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v4/swagger.json", $"Web API V4 {apiAppSettings}");
 #endif
                 //To serve the Swagger UI at the app's root (http://localhost:<port>/), set the RoutePrefix property to an empty string:
                 //c.RoutePrefix = string.Empty;
